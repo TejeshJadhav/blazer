c91a4c339e8e5e56f07196a0d04c6b12b96b011d
Dylan Dsouza <dsouzadyn@gmail.com>
2019-12-31 17:14:05 +0530

    do initial css setup

--- blazer/urls.py
+++ blazer/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from projects import views
+
 urlpatterns = [
+    path('', views.index),
     path('projects/', include('projects.urls')),
     path('admin/', admin.site.urls),
 ]
--- projects/views.py
+++ projects/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('A New Begining')
+    return render(request, 'projects/index.html', {})
